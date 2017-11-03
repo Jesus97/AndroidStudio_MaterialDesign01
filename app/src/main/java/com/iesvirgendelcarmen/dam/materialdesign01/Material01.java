4c3d0ed1c6202ab820af93de6bbfaadcf2174358
Jesus97 <jesusesma97@gmail.com>
2017-11-03 09:14:54 +0100

    2º commit

--- app/src/main/java/com/iesvirgendelcarmen/dam/materialdesign01/Material01.java
+++ app/src/main/java/com/iesvirgendelcarmen/dam/materialdesign01/Material01.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +17,11 @@ public class Material01 extends AppCompatActivity {
 
     private TextInputLayout controlNombre;
     private EditText campoNombre;
-    public Button botonAceptar;
+    private TextInputLayout controlCorreo;
+    private EditText campoCorreo;
+    private Button botonAceptar;
+    private Button botonCancelar;
+
 
 
     @Override
@@ -24,7 +29,13 @@ public class Material01 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material01);
 
+        campoNombre = (EditText) findViewById(R.id.campo_nombre);
+        campoCorreo = (EditText) findViewById(R.id.campo_correo);
+        controlNombre = (TextInputLayout) findViewById(R.id.control_nombre);
+        controlCorreo = (TextInputLayout) findViewById(R.id.control_correo);
         botonAceptar = (Button) findViewById(R.id.boton_aceptar);
+        botonCancelar = (Button) findViewById(R.id.boton_cancelar);
+
 
 
         botonAceptar.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +62,23 @@ public class Material01 extends AppCompatActivity {
             }
         });
 
+        campoCorreo.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                esCorreoValido(String.valueOf(i));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
 
     private boolean esNombreValido(String nombre){
@@ -65,10 +93,22 @@ public class Material01 extends AppCompatActivity {
         return true;
     }
 
+    private boolean esCorreoValido(String correo){
+        if(!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+            controlNombre.setError("ERROR EN CORREO");
+            return false;
+        }else{
+            controlNombre.setError(null);
+        }
+        return true;
+    }
+
     private void validarDatos(){
         String nombre = controlNombre.getEditText().getText().toString();
+        String correo = controlCorreo.getEditText().getText().toString();
         boolean a = esNombreValido(nombre);
-        if(a){
+        boolean b = esNombreValido(correo);
+        if(a&&b){
             Toast.makeText(this,"REGISTRO CORRECTO",Toast.LENGTH_LONG).show();
         }
     }
